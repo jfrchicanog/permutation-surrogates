8ce6514391f8c1076900d434e7388f226274a456
Francisco Chicano <chicano@lcc.uma.es>
2022-11-09 07:02:55 +0100

    changin saample limits and expressing node list

--- 1-launch-experiments-learning.sh
+++ 1-launch-experiments-learning.sh
@@ -45,13 +45,13 @@ orders=(${orders[@]})
 
 for ((i=0;i<\${#orders[@]};i++)); do
     COMMAND="python3 learning-surrogate.py --problem $problem --instance ${INSTANCE_DIR}/${instance} --trainingStart ${trainingStart} --trainingEnd ${trainingEnd} --trainingIncrement ${trainingIncrement} --randomSeed \${randomSeed} --irreps \${irreps[i]} --output ${OUTDIR}/${JOBNAME}-order\${orders[i]}-seed\${randomSeed}-f${date}.out"
-    echo "running: " \$COMMAND
+    echo "running: " \$COMMAND " at \$SLURM_JOB_NODELIST"
     \$COMMAND
 done
 
 EOF
 sbatch kk.sh
-rm kk.sh
+#rm kk.sh
 }
 
 configureSize() {
@@ -59,27 +59,27 @@ configureSize() {
 
     if [ "$size" == 5 ]; then
         irreps=('[5]' '[5],[4,1]' '[5],[4,1],[3,2],[3,1,1]' '[5],[4,1],[3,2],[3,1,1],[2,2,1],[2,1,1,1]' '[5],[4,1],[3,2],[3,1,1],[2,2,1],[2,1,1,1],[1,1,1,1,1]')
-        order=(0 1 2 3 4)
-        training=(2 200 2)
+        orders=(0 1 2 3 4)
+	training=(1 120 1)
     elif [ "$size" == 6 ]; then
         irreps=('[6]' '[6],[5,1]' '[6],[5,1],[4,2],[4,1,1]' '[6],[5,1],[4,2],[4,1,1],[3,3],[3,2,1],[3,1,1,1]' '[6],[5,1],[4,2],[4,1,1],[3,3],[3,2,1],[3,1,1,1],[2,2,2],[2,2,1,1],[2,1,1,1,1]')
-        order=(0 1 2 3 4)
-        training=(14 1400 14)
+        orders=(0 1 2 3 4)
+        training=(6 720 6)
     elif [ "$size" == 7 ]; then
         irreps=('[7]' '[7],[6,1]' '[7],[6,1],[5,2],[5,1,1]' '[7],[6,1],[5,2],[5,1,1],[4,3],[4,2,1],[4,1,1,1]' '[7],[6,1],[5,2],[5,1,1],[4,3],[4,2,1],[4,1,1,1],[3,3,1],[3,2,2],[3,2,1,1],[3,1,1,1,1]')
-        order=(0 1 2 3 4)
-        training=(90 9000 90)
+        orders=(0 1 2 3 4)
+        training=(50 5000 50)
     elif [ "$size" == 8 ]; then
         irreps=('[8]' '[8],[7,1]' '[8],[7,1],[6,2],[6,1,1]' '[8],[7,1],[6,2],[6,1,1],[5,3],[5,2,1],[5,1,1,1]' '[8],[7,1],[6,2],[6,1,1],[5,3],[5,2,1],[5,1,1,1],[4,4],[4,3,1],[4,2,2],[4,2,1,1],[4,1,1,1,1]')
-        order=(0 1 2)
+        orders=(0 1 2)
         training=(18 1800 18)
     elif [ "$size" == 9 ]; then
         irreps=('[9]' '[9],[8,1]' '[9],[8,1],[7,2],[7,1,1]' '[9],[8,1],[7,2],[7,1,1],[6,3],[6,2,1],[6,1,1,1]' '[9],[8,1],[7,2],[7,1,1],[6,3],[6,2,1],[6,1,1,1],[5,4],[5,3,1],[5,2,2],[5,2,1,1],[5,1,1,1,1]')
-        order=(0 1 2)
+        orders=(0 1 2)
         training=(32 3200 32)
     elif [ "$size" == 10 ]; then
         irreps=('[10]' '[10],[9,1]' '[10],[9,1],[8,2],[8,1,1]' '[10],[9,1],[8,2],[8,1,1],[7,3],[7,2,1],[7,1,1,1]' '[10],[9,1],[8,2],[8,1,1],[7,3],[7,2,1],[7,1,1,1],[6,4],[6,3,1],[6,2,2],[6,2,1,1],[6,1,1,1,1]')
-        order=(0 1 2)
+        orders=(0 1 2)
         training=(52 5200 52)
     else
         echo "Invalid size ${size}"
