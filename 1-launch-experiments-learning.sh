47f6c0147e156ca1192fa0611738f9c80718b8dc
Francisco Chicano <chicano@lcc.uma.es>
2022-11-10 11:46:39 +0100

    experiments for learning

--- 1-launch-experiments-learning.sh
+++ 1-launch-experiments-learning.sh
@@ -26,7 +26,7 @@ slurm_job() {
 #SBATCH --mem=5gb
 
 # The time the job will be running:
-#SBATCH --time=20:00:00
+#SBATCH --time=24:00:00
 
 # To use GPUs you have to request them:
 ##SBATCH --gres=gpu:1
@@ -44,7 +44,7 @@ irreps=(${irreps[@]})
 orders=(${orders[@]})
 
 for ((i=0;i<\${#orders[@]};i++)); do
-    COMMAND="python3 learning-surrogate.py --problem $problem --instance ${INSTANCE_DIR}/${instance} --trainingStart ${trainingStart} --trainingEnd ${trainingEnd} --trainingIncrement ${trainingIncrement} --randomSeed \${randomSeed} --irreps \${irreps[i]} --output ${OUTDIR}/${JOBNAME}-order\${orders[i]}-seed\${randomSeed}-f${date}.out"
+    COMMAND="python3 learning-surrogate.py --problem $problem --instance ${INSTANCE_DIR}/${instance} --trainingStart ${trainingStart} --trainingEnd ${trainingEnd} --trainingIncrement ${trainingIncrement} --randomSeed \${randomSeed} --irreps \${irreps[i]} --output ${OUTDIR}/${JOBNAME}-job${SLURM_JOB_ID}-order\${orders[i]}-seed\${randomSeed}-f${date}.out"
     echo "running: " \$COMMAND " at \$SLURM_JOB_NODELIST"
     \$COMMAND
 done
@@ -80,7 +80,7 @@ configureSize() {
     elif [ "$size" == 10 ]; then
         irreps=('[10]' '[10],[9,1]' '[10],[9,1],[8,2],[8,1,1]' '[10],[9,1],[8,2],[8,1,1],[7,3],[7,2,1],[7,1,1,1]' '[10],[9,1],[8,2],[8,1,1],[7,3],[7,2,1],[7,1,1,1],[6,4],[6,3,1],[6,2,2],[6,2,1,1],[6,1,1,1,1]')
         orders=(0 1 2)
-        training=(52 5200 52)
+        training=(52 3640 52)
     else
         echo "Invalid size ${size}"
         exit
